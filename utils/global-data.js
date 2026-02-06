54ee60cff2a618458b8ef0ccc6c1741e98144510
erfahren <erfahren@atadcrazy.net>
2026-02-05 21:49:51 -0700

    Update default global data values

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,13 +1,13 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'erfahren.info | Home';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : '';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : '';
 
   return {
     name,
@@ -15,3 +15,4 @@ export const getGlobalData = () => {
     footerText,
   };
 };
+
