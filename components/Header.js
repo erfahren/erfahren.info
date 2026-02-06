791a335501331b909284cdc3c8d1b9796bfe941c
erfahren <erfahren@atadcrazy.net>
2026-02-06 00:32:44 -0700

    Update Header component styling

--- components/Header.js
+++ components/Header.js
@@ -3,10 +3,11 @@ import Link from 'next/link';
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-      <div className="block w-12 h-12 mx-auto mb-4 rounded-full bg-conic-180 from-gradient-3 from-0% to-gradient-4 to-100%" />
+      <div className="block w-16 h-6 mx-auto mb-4 rounded-full bg-conic-120 from-gradient-2 from-0% to-gradient-8 to-60%" />
       <p className="text-2xl text-center dark:text-white">
         <Link href="/">{name}</Link>
       </p>
     </header>
   );
 }
+
